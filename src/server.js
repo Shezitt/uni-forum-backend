2249f26cff3cf6e33a9f093e3678fc18f785bc9d
Shezitt <shamirteranmustafa@gmail.com>
2025-06-26 21:53:54 -0400

    include postRoutes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -8,7 +9,10 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
