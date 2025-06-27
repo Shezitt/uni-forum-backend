8648a4e0517e768b8b2922b2e231f20b29d222f9
Shezitt <shamirteranmustafa@gmail.com>
2025-06-26 21:52:57 -0400

    update users: implement createUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,13 @@
-import { getAllUsers } from '../models/userModel.js';
+import { getAllUsers, createUser } from '../models/userModel.js';
 
 export const getUsers = async (req, res) => {
     const users = await getAllUsers();
     res.json(users);
+};
+
+export const addUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const newUser = await createUser(name, email, password);
+    res.status(201).json(newUser);
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,3 +4,11 @@ export const getAllUsers = async () => {
     const result = await pool.query('SELECT * FROM users ORDER BY created_at DESC');
     return result.rows;
 };
+
+export const createUser = async (name, email, password) => {
+    const result = await pool.query(
+        'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', 
+        [name, email, password]
+    );
+    return result.rows[0];
+};
