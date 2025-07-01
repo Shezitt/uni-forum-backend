2552c5c2225d21005ccf17a188a4defb0f5b3c7f
Shezitt <shamirteranmustafa@gmail.com>
2025-07-01 19:19:02 -0400

    add Joi validation middleware and apply to createReply route

---
+++ src/validations/replyValidation.js
@@ -0,0 +1,6 @@
+import Joi from 'joi';
+
+export const createReplySchema = Joi.object({
+    content: Joi.string().min(1).max(1000).required(),
+    author_id: Joi.number().integer().positive().required()
+});
