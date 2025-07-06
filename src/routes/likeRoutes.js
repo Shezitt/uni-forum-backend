47e7cdbd66a8648a774d067736d2afe5c1f8e9da
Shezitt <shamirteranmustafa@gmail.com>
2025-07-05 20:38:51 -0400

    add endpoints to get like counts for posts and replies

--- src/routes/likeRoutes.js
+++ src/routes/likeRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
-import { likePost, likeReply } from '../controllers/likeController.js';
+import { likePost, likeReply, getPostLikeCount, getReplyLikeCount } from '../controllers/likeController.js';
 
 const router = express.Router();
 
 router.post('/posts/:postId/like', authenticateToken, likePost);
 router.post('/posts/:postId/replies/:replyId/like', authenticateToken, likeReply);
+router.get('/posts/:postId/likes/count', getPostLikeCount);
+router.get('/posts/:postId/replies/:replyId/likes/count', getReplyLikeCount);
 
 export default router;
