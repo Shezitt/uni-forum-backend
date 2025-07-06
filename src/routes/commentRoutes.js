ba080687eaac117d84bec3e660ebd9e9de36f3a5
Shezitt <shamirteranmustafa@gmail.com>
2025-07-05 22:21:42 -0400

    add validation to create comments, reusing createReplySchema

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authenticateToken } from '../middlewares/authMiddleware.js';
 import { addComment, getComments } from '../controllers/commentController.js';
+import { validateBody } from '../middlewares/validateMiddleware.js';
+import { createReplySchema } from '../validations/replyValidation.js';
 
 const router = express.Router();
 
-router.post('/posts/:postId/replies/:replyId/comments', authenticateToken, addComment);
+router.post('/posts/:postId/replies/:replyId/comments', authenticateToken, validateBody(createReplySchema), addComment);
 router.get('/posts/:postId/replies/:replyId/comments', getComments);
 
 export default router;
